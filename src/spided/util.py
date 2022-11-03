92d56e7ca42f55f745293ab5915bf0b8449f7197
mondayfirst <50545012+mondayfirst@users.noreply.github.com>
2022-11-03 22:01:37 +0800

    Fix small bug

--- src/spided/util.py
+++ src/spided/util.py
@@ -3,9 +3,12 @@ import logging
 import os
 import re
 
-logging.basicConfig(level=logging.ERROR, format='%(name)s %(asctime)s %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(level=logging.ERROR,
+                    format='%(name)s %(asctime)s %(levelname)s: %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger(os.path.splitext(os.path.basename(__file__))[0])
 logger.setLevel(logging.DEBUG)
+
 
 def get_final_path_from_url(url):
     return url[url.rfind('/') + 1:]
@@ -13,15 +16,17 @@ def get_final_path_from_url(url):
 
 def is_web_file_from_url(url):
     last_text = url.split("/")[-1]  # 获取URL里最后一节字符串，用于判断是文件还是目录
-    if ("." in last_text) and (re.search("(?:\.hdf)|(?:\.[a-z]{3,4})") is not None):  # 判断是url是否是文件夹链接
+    if ("." in last_text) and (re.search("(?:\.hdf)|(?:\.[a-z]{3,4})", url) is not None):  # 判断是url是否是文件夹链接
         return True
     else:
         return False
+
 
 def unzip(filepath, objdir):
     if filepath.endswith(".zip"):
         azip = zipfile.ZipFile(filepath)
         azip.extractall(objdir)
+
 
 def get_temp_dir(objfilepath, zipdirname="zipfiles"):
     dir_path = os.path.dirname(objfilepath)
@@ -33,7 +38,8 @@ def get_temp_dir(objfilepath, zipdirname="zipfiles"):
     else:
         return dir_path
 
-def compare_filesize(web_file_name:str, web_file_size:int, localdir:str="./") -> bool:
+
+def compare_filesize(web_file_name: str, web_file_size: int, localdir: str = "./") -> bool:
     """检查是否下载完成.
 
     Args:
